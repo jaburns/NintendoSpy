a754cfbc7bb7f0ab2dc8d92f514352adc77dfbe2
Jeremy Burns <jeremyaburns@gmail.com>
2014-04-14 19:40:20 -0600

    Switched to viewmodel based system for setupwindow

--- wpf-update/SetupWindow.xaml.cs
+++ wpf-update/SetupWindow.xaml.cs
@@ -1,65 +1,77 @@
 ﻿using System;
-using System.IO.Ports;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace NintendoSpy
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class SetupWindow : Window
     {
-        public MainWindow()
+        SetupWindowViewModel _vm;
+        DispatcherTimer _portListUpdateTimer;
+
+        public SetupWindow()
         {
             InitializeComponent();
-            populatePortList ();
-            addSkin ("Default N64");
-            addSkin ("Default GameCube");
+            _vm = new SetupWindowViewModel ();
+            DataContext = _vm;
+
+            _vm.AddSkin (new SetupWindowViewModel.SkinEntry { Name = "Default N64" });
+            _vm.AddSkin (new SetupWindowViewModel.SkinEntry { Name = "Default GameCube" });
+
+            _portListUpdateTimer = new DispatcherTimer ();
+            _portListUpdateTimer.Interval = TimeSpan.FromSeconds (1);
+            _portListUpdateTimer.Tick += portListUpdateTimer_Tick;
+            _portListUpdateTimer.Start ();
         }
 
-        void populatePortList()
-        {
-            var lastValue = "";
-            if (portList.SelectedItem != null) {
-                lastValue = (portList.SelectedItem as ComboBoxItem).Content as String;
-            }
-
-            portList.Items.Clear();
-            portList.SelectedIndex = -1;
-
-            string[] ports = SerialPort.GetPortNames();
-            for( int i = 0; i < ports.Length; ++i ) 
-            {
-                var newItem = new ComboBoxItem ();
-                newItem.Content = ports[i];
-                portList.Items.Add (newItem);
-
-                if (lastValue == ports[i]) {
-                    portList.SelectedIndex = i;
-                }
-            }
-
-            if (portList.SelectedIndex < 0) portList.SelectedIndex = 0;
+        void portListUpdateTimer_Tick (object sender, EventArgs e) {
+            _vm.UpdatePortList ();
         }
 
-        void addSkin (string name) 
-        {
-            var item = new ListBoxItem ();
-            item.Content = name;
-            item.Foreground = new SolidColorBrush (Color.FromArgb (0xFF,0xCB,0xCB,0xCB));
-            skinsList.Items.Add (item);
+        void goButton_Click (object sender, RoutedEventArgs e) {
+
+        }
+    }
+
+    public class SetupWindowViewModel 
+    {
+        public struct SkinEntry {
+            public string Name { get; set; }
+            public string Path { get; set; }
+        }
+
+        List <string> _ports = new List <String> ();
+        List <SkinEntry> _skins = new List <SkinEntry> ();
+
+        CollectionView _portsView;
+        CollectionView _skinsView;
+
+        public CollectionView Ports { get { return _portsView; } }
+        public CollectionView Skins { get { return _skinsView; } }
+
+        public string SelectedPort { get; set; }
+        public string SelectedSkinPath { get; set; }
+
+        public SetupWindowViewModel () {
+            _portsView = new CollectionView (_ports);
+            _skinsView = new CollectionView (_skins);
+        }
+
+        public void UpdatePortList () {
+            _ports.Clear ();
+            _ports.AddRange (SerialPort.GetPortNames ());
+            _portsView.Refresh ();
+        }
+
+        public void AddSkin (SkinEntry skin) {
+            _skins.Add (skin);
+            _skinsView.Refresh ();
         }
     }
 }
+
