f354df3868061f179d96942924494c92376b74bf
Jeremy Burns <jeremyaburns@gmail.com>
2014-04-20 01:43:08 -0600

    Added mode detection and 3-wire snes read mode

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // NintendoSpy Firmware for Arduino
-// Written by Jeremy A Burns
+// Written by jaburns
 
 
 // ---------- Uncomment one of these options to select operation mode --------------
@@ -8,13 +8,21 @@
 //#define MODE_N64
 //#define MODE_SNES
 //#define MODE_NES
-//#define MODE_SWITCH_SNES_GC
+#define MODE_DETECT
+// ---------------------------------------------------------------------------------
+//#define MODE_2WIRE_SNES
 // ---------------------------------------------------------------------------------
 
 
-#define PIN_READ( pin ) (PIND&(1<<(pin)))
+#define PIN_READ( pin )  (PIND&(1<<(pin)))
+#define PINC_READ( pin ) (PINC&(1<<(pin)))
 #define MICROSECOND_NOPS "nop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\nnop\n"
 
+#define WAIT_FALLING_EDGE( pin ) while( !PIN_READ(pin) ); while( PIN_READ(pin) );
+
+#define MODEPIN_SNES 0
+#define MODEPIN_N64  1
+#define MODEPIN_GC   2
 
 #define N64_PIN        2
 #define N64_PREFIX     9
@@ -22,13 +30,13 @@
 
 #define SNES_LATCH      3
 #define SNES_DATA       4
+#define SNES_CLOCK      6
 #define SNES_BITCOUNT  16
 #define  NES_BITCOUNT   8
 
 #define GC_PIN        5
 #define GC_PREFIX    25
 #define GC_BITCOUNT  64
-
 
 #define ZERO  '\0'  // Use a byte value of 0x00 to represent a bit with value 0.
 #define ONE    '1'  // Use an ASCII one to represent a bit with value 1.  This makes Arduino debugging easier.
@@ -45,6 +53,8 @@ void setup()
 {
     PORTD = 0x00;
     DDRD  = 0x00;
+    PORTC = 0xFF; // Set the pull-ups on the port we use to check operation mode.
+    DDRC  = 0x00;
     Serial.begin( 115200 );
 }
 
@@ -61,43 +71,41 @@ void read_oneWire( unsigned char bits )
 read_loop:
 
     // Wait for the line to go high then low.
-    while( !PIN_READ(pin) );
-    while(  PIN_READ(pin) );
-            
+    WAIT_FALLING_EDGE( pin );
+
     // Wait ~2us between line reads
     asm volatile( MICROSECOND_NOPS MICROSECOND_NOPS );
-    
+
     // Read a bit from the line and store as a byte in "rawData"
     *rawDataPtr = PIN_READ(pin);
     ++rawDataPtr;
     if( --bits == 0 ) return;
-    
+
     goto read_loop;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Performs a read cycle from a shift register based controller.
-// This includes the NES and the SNES.
+// Performs a read cycle from a shift register based controller (SNES + NES) using only the data and latch
+// wires, and waiting a fixed time between reads.  This read method is deprecated due to being finicky,
+// but still exists here to support older builds.
 //     latch = Pin index on Port D where the latch wire is attached.
 //     data  = Pin index on Port D where the output data wire is attached.
 //     bits  = Number of bits to read from the controller.
 //  longWait = The NES takes a bit longer between reads to get valid results back.
 template< unsigned char latch, unsigned char data, unsigned char longWait >
-void read_shiftRegister( unsigned char bits )
-{ 
+void read_shiftRegister_2wire( unsigned char bits )
+{
     unsigned char *rawDataPtr = rawData;
-    
-    // Wait for a falling edge on the latch pin.
-    while( !PIN_READ(latch) );
-    while(  PIN_READ(latch) );
+
+    WAIT_FALLING_EDGE( latch );
 
 read_loop:
-    
+
     // Read the data from the line and store in "rawData"
     *rawDataPtr = !PIN_READ(data);
     ++rawDataPtr;
-    if( --bits == 0 ) return; 
-    
+    if( --bits == 0 ) return;
+
     // Wait until the next button value is on the data line. ~12us between each.
     asm volatile(
         MICROSECOND_NOPS MICROSECOND_NOPS MICROSECOND_NOPS MICROSECOND_NOPS
@@ -116,14 +124,34 @@ read_loop:
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Preferred method for reading SNES + NES controller data.
+template< unsigned char latch, unsigned char data, unsigned char clock >
+void read_shiftRegister( unsigned char bits )
+{
+    unsigned char *rawDataPtr = rawData;
+
+    WAIT_FALLING_EDGE( latch );
+
+    do {
+        WAIT_FALLING_EDGE( clock );
+        *rawDataPtr = !PIN_READ(data);
+        ++rawDataPtr;
+    }
+    while( --bits > 0 );
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Sends a packet of controller data over the Arduino serial interface.
 inline void sendRawData( unsigned char first, unsigned char count )
 {
     for( unsigned char i = first ; i < first + count ; i++ ) {
-        Serial.print( rawData[i] ? ONE : ZERO );
+        Serial.write( rawData[i] ? ONE : ZERO );
     }
-    Serial.print( SPLIT );
+    Serial.write( SPLIT );
 }
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Update loop definitions for the various console modes.
@@ -147,7 +175,11 @@ inline void loop_N64()
 inline void loop_SNES()
 {
     noInterrupts();
-    read_shiftRegister< SNES_LATCH , SNES_DATA , false >( SNES_BITCOUNT );
+#ifdef MODE_2WIRE_SNES
+    read_shiftRegister_2wire< SNES_LATCH , SNES_DATA , false >( SNES_BITCOUNT );
+#else
+    read_shiftRegister< SNES_LATCH , SNES_DATA , SNES_CLOCK >( SNES_BITCOUNT );
+#endif
     interrupts();
     sendRawData( 0 , SNES_BITCOUNT );
 }
@@ -155,9 +187,13 @@ inline void loop_SNES()
 inline void loop_NES()
 {
     noInterrupts();
+#ifdef MODE_2WIRE_SNES
     read_shiftRegister< SNES_LATCH , SNES_DATA , true >( NES_BITCOUNT );
+#else
+    read_shiftRegister< SNES_LATCH , SNES_DATA , SNES_CLOCK >( SNES_BITCOUNT );
+#endif
     interrupts();
-    sendRawData( 0 , NES_BITCOUNT );  
+    sendRawData( 0 , NES_BITCOUNT );
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -165,18 +201,22 @@ inline void loop_NES()
 void loop()
 {
 #ifdef MODE_GC
-    loop_GC();    
+    loop_GC();
 #elif defined MODE_N64
     loop_N64();
 #elif defined MODE_SNES
     loop_SNES();
 #elif defined MODE_NES
     loop_NES();
-#elif defined MODE_SWITCH_SNES_GC
-    if( PIN_READ(7) ) { 
+#elif defined MODE_DETECT
+    if( PINC_READ( MODEPIN_SNES ) ) {
         loop_SNES();
-    } else {
+    } else if( PINC_READ( MODEPIN_N64 ) ) {
+        loop_N64();
+    } else if( PINC_READ( MODEPIN_GC ) ) {
         loop_GC();
+    } else {
+        loop_NES();
     }
 #endif
 }
