1032fec18ca49920f7d798acb7d2c0f470dcde7c
Jeremy Burns <jeremyaburns@gmail.com>
2014-04-19 21:58:42 -0600

    Added build script

---
+++ release.bat
@@ -0,0 +1,18 @@
+@echo off
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe NintendoSpy.sln /p:Configuration=Release
+
+if errorlevel 0 goto buildOK
+echo Aborting release. Error during build.
+goto end
+
+:buildOK
+del release\NintendoSpy.zip
+cd bin\Release
+"C:\Program Files\7-Zip\7z.exe" a ..\..\release\NintendoSpy.zip SlimDX.dll
+"C:\Program Files\7-Zip\7z.exe" a ..\..\release\NintendoSpy.zip NintendoSpy.exe
+cd ..\..
+"C:\Program Files\7-Zip\7z.exe" a release\NintendoSpy.zip skins
+
+:end
+pause
