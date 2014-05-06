1b1fe322ff885b8c4e0e0498a726fd55831e5c46
Jeremy Burns <jeremyaburns@gmail.com>
2014-05-05 23:35:19 -0600

    Removed release from repo

--- release.bat
+++ release.bat
@@ -7,18 +7,18 @@ echo Aborting release. Error during build.
 goto end
 
 :buildOK
-del release\NintendoSpy.zip
+del NintendoSpy-release.zip
 
 cd bin\Release
-"C:\Program Files\7-Zip\7z.exe" a ..\..\release\NintendoSpy.zip NintendoSpy.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\NintendoSpy-release.zip NintendoSpy.exe
 cd ..\..
 
 cd slimdx\x64
-"C:\Program Files\7-Zip\7z.exe" a ..\..\release\NintendoSpy.zip SlimDX.dll
+"C:\Program Files\7-Zip\7z.exe" a ..\..\NintendoSpy-release.zip SlimDX.dll
 cd ..\..
 
-"C:\Program Files\7-Zip\7z.exe" a release\NintendoSpy.zip skins
-"C:\Program Files\7-Zip\7z.exe" a release\NintendoSpy.zip keybindings.xml
+"C:\Program Files\7-Zip\7z.exe" a NintendoSpy-release.zip skins
+"C:\Program Files\7-Zip\7z.exe" a NintendoSpy-release.zip keybindings.xml
 
 :end
 pause
