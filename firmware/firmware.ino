b69786ca07cd1b7cefa3c612aed1d12483fe0849
Christopher J. Mallery <christopher.mallery@gmail.com>
2018-04-23 20:55:35 -0700

    Deleted a line on accident

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -21,6 +21,7 @@
 // ---------------------------------------------------------------------------------
 // The only reason you'd want to use 2-wire SNES mode is if you built a NintendoSpy
 // before the 3-wire firmware was implemented.  This mode is for backwards
+// compatibility only. 
 //#define MODE_2WIRE_SNES
 // ---------------------------------------------------------------------------------
 // Uncomment this for MODE_SEGA, MODE_CLASSIC and MODE_BOOSTER_GRIP serial debugging output
