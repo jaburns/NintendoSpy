1f4ff8a2920ea14e6628a10b85e7c266dc33baed
Christopher J Mallery <christopher.mallery@gmail.com>
2018-12-08 07:13:43 -0800

    unified the readers for saturn digital and analog

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -408,6 +408,59 @@ inline void read_SSData()
 
 }
 
+inline void sendRawSSDataV2()
+{
+    #ifndef DEBUG
+    for(int i = 0; i < 8;++i)
+    {
+      Serial.write(i == 6 ? ONE : ZERO);
+    }
+    Serial.write((ssState3 & 0b00100000) ? ZERO : ONE);
+    Serial.write((ssState3 & 0b00010000) ? ZERO : ONE);
+    Serial.write((ssState3 & 0b00001000) ? ZERO : ONE);
+    Serial.write((ssState3 & 0b00000100) ? ZERO : ONE);
+
+    Serial.write((ssState2 & 0b00100000) ? ZERO : ONE);
+    Serial.write((ssState2 & 0b00010000) ? ZERO : ONE);
+    Serial.write((ssState2 & 0b00001000) ? ZERO : ONE);
+    Serial.write((ssState2 & 0b00000100) ? ZERO : ONE);
+    
+    Serial.write ((ssState1 & 0b00100000) ? ZERO : ONE );
+    Serial.write ((ssState1 & 0b00010000) ? ZERO : ONE );
+    Serial.write ((ssState1 & 0b00001000) ? ZERO : ONE );
+    Serial.write ((ssState1 & 0b00000100)  ? ZERO : ONE );
+
+    Serial.write ((ssState4 & 0b00100000) ? ZERO : ONE );
+    Serial.write (ONE);
+    Serial.write (ONE);
+    Serial.write (ONE);
+
+    for(int i = 0; i < 32;++i)
+      Serial.write(ZERO);
+
+    Serial.write( SPLIT );
+    #else 
+    Serial.print((ssState1 & 0b00000100)    ? "Z" : "0");
+    Serial.print((ssState1 & 0b00001000)    ? "Y" : "0");
+    Serial.print((ssState1 & 0b00010000)    ? "X" : "0");
+    Serial.print((ssState1 & 0b00100000)    ? "R" : "0");
+
+    Serial.print((ssState2 & 0b00000100)    ? "B" : "0");
+    Serial.print((ssState2 & 0b00001000)    ? "C" : "0");
+    Serial.print((ssState2 & 0b00010000)    ? "A" : "0");
+    Serial.print((ssState2 & 0b00100000)    ? "S" : "0");
+
+    Serial.print((ssState3 & 0b00000100)    ? "u" : "0");
+    Serial.print((ssState3 & 0b00001000)    ? "d" : "0");
+    Serial.print((ssState3 & 0b00010000)    ? "l" : "0");
+    Serial.print((ssState3 & 0b00100000)    ? "r" : "0");
+    
+    Serial.print((ssState4 & 0b00100000)    ? "L" : "0");
+ 
+    Serial.print("\n");
+    #endif
+}
+
 inline void sendRawSSData()
 {
     #ifndef DEBUG
@@ -963,7 +1016,7 @@ inline void loop_SS()
   noInterrupts();
   read_SSData();
   interrupts();
-  sendRawSSData();
+  sendRawSSDataV2();
 }
 
 inline void loop_SS3D()
