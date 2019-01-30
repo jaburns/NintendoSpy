63b1867288f304db1277a95b7bf227d4c5a5c8e8
Zoggins <zoggins@gmail.com>
2019-01-30 06:37:29 -0800

    included an AnyCPU exe

--- release.bat
+++ release.bat
@@ -1,26 +1,39 @@
 @echo off
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe RetroSpy.sln /p:Configuration=Release /p:Platform=x86
 
-if errorlevel 0 goto buildOK
-echo Aborting release. Error during build.
+if errorlevel 0 goto x64Build
+echo Aborting release. Error during x86 build.
 goto end
 
+:x64Build
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe RetroSpy.sln /p:Configuration=Release /p:Platform=x64
 
+if errorlevel 0 goto AnyCPUBuild
+echo Aborting release. Error during x64 build.
+goto end
+
+:AnyCPUBuild
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe RetroSpy.sln /p:Configuration=Release /p:Platform="Any CPU"
+
 if errorlevel 0 goto buildOK
-echo Aborting release. Error during build.
+echo Aborting release. Error during AnyCPU build.
 goto end
 
 :buildOK
 del RetroSpy-release.zip
 
+cd bin\Release
+"C:\Program Files\7-Zip\7z.exe" a ..\..\RetroSpy-release.zip RetroSpy.exe
+cd ..\..
+
 cd bin\x64\Release
-"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.exe
+copy RetroSpy.exe RetroSpy.x64.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.x64.exe
 cd ..\..\..
 
 cd bin\x86\Release
-copy RetroSpy.exe RetroSpy32.exe
-"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy32.exe
+copy RetroSpy.exe RetroSpy.x86.exe
+"C:\Program Files\7-Zip\7z.exe" a ..\..\..\RetroSpy-release.zip RetroSpy.x86.exe
 cd ..\..\..
 
 ;cd SharpDX\net45
