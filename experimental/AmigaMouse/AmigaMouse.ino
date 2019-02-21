fb6905cb2d25c1e303a9a2898370c285591067e5
Chris Mallery <chmaller@microsoft.com>
2019-02-21 10:15:59 -0800

    Moved Amiga Mouse

--- experimental/AmigaMouse/AmigaMouse.ino
+++ experimental/AmigaMouse/AmigaMouse.ino
@@ -3,14 +3,21 @@
 // v1.0
 // RetroSpy written by zoggins
 
+// -------------------------------------------------------------
+//    Mouse distance calculations are based on the video clock
+//    Uncomment out PAL or NTSC as appropriate.
+// -------------------------------------------------------------
+//#define PAL
+//#define NTSC
+
 // ---------- Uncomment these for debugging modes --------------
 //#define DEBUG
 
 byte buttons[3];
-byte currentX;
-byte currentY;
-byte lastX;
-byte lastY;
+int8_t currentX;
+int8_t currentY;
+int8_t lastX;
+int8_t lastY;
 byte lastEncoderX;
 byte lastEncoderY;
 
@@ -18,28 +25,34 @@ void setup()
 {
   currentX = lastX = 0;
   currentY = lastY = 0;
-
-  // ~16ms timer interrupt
-  TIMSK2 = (TIMSK2 & B11111110) | 0x01;
-  TCCR2B = (TCCR2B & B11111000) | 0x07;
+  
+  // TIMER 1 for interrupt frequency 50 or 60 Hz:
+  cli(); // stop interrupts
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
+  TCNT1  = 0; // initialize counter value to 0
+#ifdef NTSC
+  // set compare match register for 60.00060000600006 Hz increments
+  OCR1A = 33332; // = 16000000 / (8 * 60.00060000600006) - 1 (must be <65536)
+#else // PAL
+  // set compare match register for 50 Hz increments
+  OCR1A = 39999; // = 16000000 / (8 * 50) - 1 (must be <65536)
+#endif
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12, CS11 and CS10 bits for 8 prescaler
+  TCCR1B |= (0 << CS12) | (1 << CS11) | (0 << CS10);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+  sei(); // allow interrupts
 
   Serial.begin( 115200 );
 }
 
-ISR(TIMER2_OVF_vect){
+ISR(TIMER1_COMPA_vect){
   int8_t x = lastX - currentX;
   int8_t y = lastY - currentY; 
 
-  if (x > 127)
-    x = (255 - x) * -1;
-  if (y > 127)
-    y = (255 - y) * -1;
-
-  if (x < -127)
-    x = (255 + x);
-  if (y < -127)
-    y = (255 + y);
-    
   lastX = currentX;
   lastY = currentY;
   
@@ -49,8 +62,10 @@ ISR(TIMER2_OVF_vect){
   Serial.print('|');
   Serial.print(x);
   Serial.print('|');
-  Serial.println(y);
+  Serial.print(y);
   Serial.print('\n');
+  xminusticks = 0;
+  xplusticks = 0;
 #else
   for(int i = 0; i < 3; ++i)
     Serial.write(buttons[i]);
@@ -60,7 +75,6 @@ ISR(TIMER2_OVF_vect){
     Serial.write( (y & (1 << i)) == 0 ? 0 : 1);
   Serial.write('\n');
 #endif
-
 
 }
 
@@ -108,6 +122,7 @@ void loop() {
   {
     ++currentY;
     lastEncoderY = yData;
+
   }
   else if ((lastEncoderY == 0 && yData == 16)
        || (lastEncoderY == 16 && yData == 20)
