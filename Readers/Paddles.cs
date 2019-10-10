ed0cfb72e10bb85331e40bd8d466325d05b7cd0f
Christopher J. Mallery <christopher.mallery@gmail.com>
2019-10-09 20:53:09 -0700

    Paddles on Atari actually work.

--- Readers/Paddles.cs
+++ Readers/Paddles.cs
@@ -8,31 +8,14 @@ namespace RetroSpy.Readers
 {
     static public class Paddles
     {
+        static byte lastLeft = 0;
+        static byte lastRight = 0;
 
-        const int PACKET_SIZE = 4;
+        const int PACKET_SIZE = 5;
 
         static readonly string[] BUTTONS = {
             "2", "1"
         };
-
-        static readonly string[] ANALOG_POSITIONS_LEFT =
-        {
-            "left_0", "left_1", "left_2", "left_3", "left_4", "left_5", "left_6", "left_7", "left_8", "left_9", "left_10",
-            "left_11", "left_12", "left_13", "left_14", "left_15", "left_16", "left_17", "left_18", "left_19", "left_20",
-            "left_21", "left_22", "left_23", "left_24", "left_25", "left_26", "left_27", "left_28", "left_29", "left_30",
-            "left_31", "left_32", "left_33", "left_34", "left_35", "left_36", "left_37", "left_38", "left_39", "left_40",
-            "left_41", "left_42", "left_43", "left_45", "left_45", "left_46", "left_47", "left_48", "left_49", "left_50", "left_51"
-        };
-
-        static readonly string[] ANALOG_POSITIONS_RIGHT =
-        {
-            "right_0", "right_1", "right_2", "right_3", "right_4", "right_5", "right_6", "right_7", "right_8", "right_9", "right_10",
-            "right_11", "right_12", "right_13", "right_14", "right_15", "right_16", "right_17", "right_18", "right_19", "right_20",
-            "right_21", "right_22", "right_23", "right_24", "right_25", "right_26", "right_27", "right_28", "right_29", "right_30",
-            "right_31", "right_32", "right_33", "right_34", "right_35", "right_36", "right_37", "right_38", "right_39", "right_40",
-            "right_41", "right_42", "right_43", "right_45", "right_45", "right_46", "right_47", "right_48", "right_49", "right_50", "right_51"
-        };
-
 
         static float readPaddle(ushort input)
         {
@@ -43,6 +26,8 @@ namespace RetroSpy.Readers
         {
            
             if (packet.Length < PACKET_SIZE) return null;
+            if (packet[4] != 5) return null;
+            if (packet[2] == 0 || packet[3] == 0) return null;
 
             var state = new ControllerStateBuilder();
 
@@ -52,18 +37,21 @@ namespace RetroSpy.Readers
                 state.SetButton(BUTTONS[i], packet[i] != 0x00);
             }
 
-            for(int i = 0; i < ANALOG_POSITIONS_LEFT.Length; ++i)
-            {
-                state.SetButton(ANALOG_POSITIONS_LEFT[i], packet[2]/5 == i );
-            }
+            byte left = packet[2];
+            byte right = packet[3];
 
-            for (int i = 0; i < ANALOG_POSITIONS_RIGHT.Length; ++i)
-            {
-                state.SetButton(ANALOG_POSITIONS_RIGHT[i], packet[3]/5 == i);
-            }
+            if (Math.Abs(left - lastLeft) > 10)
+                lastLeft = left;
+            else
+                left = lastLeft;
 
-            state.SetAnalog("left", readPaddle(packet[2]));
-            state.SetAnalog("right", readPaddle(packet[3]));
+            if (Math.Abs(right - lastRight) > 10)
+                lastRight = right;
+            else
+                right = lastRight;
+
+            state.SetAnalog("left", readPaddle(left));
+            state.SetAnalog("right", readPaddle(right));
 
             return state.Build();
         }
--- experimental/Paddles/Paddles.ino
+++ experimental/Paddles/Paddles.ino
@@ -16,15 +16,24 @@
 #define PIN_READ( pin )  (PIND&(1<<(pin)))
 
 // The below values are not scientific. These will need to be tuned for different sets of paddles.
-float EMA_al = .08;      //initialization of EMA alpha left
-float EMA_ar = .08;      //initialization of EMA alpha right
-int nominal_left_min = 87;
-int nominal_right_min = 110;
-int nominal_left_max = 1007;
-int nominal_right_max = 1009;
+float EMA_al = 1;      //initialization of EMA alpha left
+float EMA_ar = 1;      //initialization of EMA alpha right
+int nominal_left_min = 213;
+int nominal_right_min = 207;
+int nominal_left_max = 1004;
+int nominal_right_max = 1003;
+
+int lastLeft = 0;
+bool leftAscending = false;
+int currentLeft = 0;
+
+int lastRight = 0;
+bool rightAscending = false;
+int currentRight = 0;
 
 int EMA_Sl = 0;          //initialization of EMA S left
 int EMA_Sr = 0;          //initialization of EMA S right
+
 
 static int ScaleInteger(float oldValue, float oldMin, float oldMax, float newMin, float newMax)
 {
@@ -42,36 +51,64 @@ void setup() {
   for (int i = 2; i <= 8; ++i)
     pinMode(i, INPUT_PULLUP);
 
-  EMA_Sl = analogRead(0);
-  delay(2);
-  EMA_Sl = analogRead(0);
-  delay(2);
-  EMA_Sr = analogRead(1);
-  delay(2);
-  EMA_Sr = analogRead(1);
+  EMA_Sl = currentLeft = analogRead(0);
+  EMA_Sr = currentRight = analogRead(1);
 
   Serial.begin( 115200 );
 }
 
 void loop() {
 
+
   noInterrupts();
   byte pins = 0;
   pins |= (PIND >> 2);
   int leftPaddle = analogRead(0);
-  delay(2);
-  leftPaddle = analogRead(0);
-  delay(2);
   int rightPaddle = analogRead(1);
-  delay(2);
-  rightPaddle = analogRead(1);
   interrupts();
 
+  if (leftAscending && leftPaddle < lastLeft)
+  {
+    leftAscending = false;
+    currentLeft = lastLeft;
+    lastLeft = leftPaddle;
+    leftPaddle = currentLeft;
+  }
+  else if (!leftAscending && leftPaddle > lastLeft)
+  {
+    leftAscending = true;
+    lastLeft = leftPaddle;
+    leftPaddle = currentLeft;
+  }
+  else
+  {
+    lastLeft = leftPaddle;
+    leftPaddle = currentLeft;
+  }
+
+  if (rightAscending && rightPaddle < lastRight)
+  {
+    rightAscending = false;
+    currentRight = lastRight;
+    lastRight = rightPaddle;
+    rightPaddle = currentRight;
+  }
+  else if (!rightAscending && leftPaddle > lastLeft)
+  {
+    rightAscending = true;
+    lastRight = rightPaddle;
+    rightPaddle = currentRight;
+  }
+  else
+  {
+    lastRight = rightPaddle;
+    rightPaddle = currentRight;
+  }
+      
   byte fire1 = ((pins & 0b0000000000000100) == 0);
   byte fire2 = ((pins & 0b0000000000001000) == 0);
   EMA_Sl = (EMA_al*leftPaddle) + ((1-EMA_al)*EMA_Sl); 
   EMA_Sr = (EMA_ar*rightPaddle) + ((1-EMA_ar)*EMA_Sr);
-
 #ifdef DEBUG
     Serial.print(fire1 ? "3" : "-");
     Serial.print(fire2 ? "4" : "-");
@@ -85,12 +122,14 @@ void loop() {
     Serial.print(ScaleInteger(EMA_Sr, nominal_right_min, nominal_right_max, 0, 255));
     Serial.print("\n");
 #else
+    int sil = ScaleInteger(EMA_Sl, nominal_left_min, nominal_left_max, 0, 255);
+    int sir = ScaleInteger(EMA_Sr, nominal_right_min, nominal_right_max, 0, 255);
     Serial.write(fire1);
     Serial.write(fire2);
-    Serial.write(ScaleInteger(EMA_Sl, nominal_left_min, nominal_left_max, 0, 255));
-    Serial.write(ScaleInteger(EMA_Sr, nominal_right_min, nominal_right_max, 0, 255));
+    Serial.write(sil);
+    Serial.write(sir);
+    Serial.write(5);
     Serial.write('\n');
 #endif
 
-  delay(2);
 }
