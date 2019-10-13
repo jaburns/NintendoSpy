d1339d87d3e02894f602e3688fa47d615a613b6e
Christopher J. Mallery <christopher.mallery@gmail.com>
2019-10-13 13:55:23 -0700

    Double paddle skin and code

--- Readers/Paddles.cs
+++ Readers/Paddles.cs
@@ -11,32 +11,40 @@ namespace RetroSpy.Readers
 
         const int PACKET_SIZE = 6;
 
-        static readonly string[] BUTTONS = {
-            null, "fire"
-        };
-
         static float readPaddle(ushort input)
         {
-            return (float)(input)/256;
+            return (float)(input) / 256;
         }
-        
+
+        static bool SecondButton;
+        static float SecondPaddle;
+
         static public ControllerState ReadFromPacket(byte[] packet)
         {
-           
+
             if (packet.Length < PACKET_SIZE) return null;
             if (packet[4] != 5) return null;
 
             var state = new ControllerStateBuilder();
 
-            for (int i = 0; i < BUTTONS.Length; ++i)
-            {
-                if (string.IsNullOrEmpty(BUTTONS[i])) continue;
-                state.SetButton(BUTTONS[i], packet[i] != 0x00);
-            }
-
             state.SetAnalog("paddle", readPaddle(packet[2]));
-           
+            state.SetAnalog("paddle2", SecondPaddle);
+            state.SetButton("fire2", SecondButton);
+            state.SetButton("fire", (packet[1] != 0x00));
+
             return state.Build();
+        }
+
+        static public ControllerState ReadFromSecondPacket(byte[] packet)
+        {
+
+            if (packet.Length < PACKET_SIZE) return null;
+            if (packet[4] != 5) return null;
+
+            SecondButton = (packet[1] != 0x00);
+            SecondPaddle = readPaddle(packet[2]);
+
+            return null;
         }
     }
 }
