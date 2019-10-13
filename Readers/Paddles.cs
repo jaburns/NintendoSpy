263f3371dceb98515be1c2be2ecf16b667660dc2
Christopher J. Mallery <christopher.mallery@gmail.com>
2019-10-12 21:42:11 -0700

    Breakthrough in paddles for Atari 2600 and 7800 in 2600 mode.

--- Readers/Paddles.cs
+++ Readers/Paddles.cs
@@ -8,13 +8,11 @@ namespace RetroSpy.Readers
 {
     static public class Paddles
     {
-        static byte lastLeft = 0;
-        static byte lastRight = 0;
 
         const int PACKET_SIZE = 6;
 
         static readonly string[] BUTTONS = {
-            "2", "1"
+            null, "1"
         };
 
         static float readPaddle(ushort input)
@@ -22,12 +20,13 @@ namespace RetroSpy.Readers
             return (float)(input)/256;
         }
 
+        static byte lastVal;
+
         static public ControllerState ReadFromPacket(byte[] packet)
         {
            
             if (packet.Length < PACKET_SIZE) return null;
             if (packet[4] != 5) return null;
-            if (packet[2] == 0 || packet[3] == 0) return null;
 
             var state = new ControllerStateBuilder();
 
@@ -37,22 +36,8 @@ namespace RetroSpy.Readers
                 state.SetButton(BUTTONS[i], packet[i] != 0x00);
             }
 
-            byte left = packet[2];
-            byte right = packet[3];
-
-            if ((Math.Abs(left - lastLeft)/256.0)*100.0 > (packet[5] - 11))
-                lastLeft = left;
-            else
-                left = lastLeft;
-
-            if ((Math.Abs(right - lastRight) / 256.0) * 100.0 > (packet[5] - 11))
-                lastRight = right;
-            else
-                right = lastRight;
-
-            state.SetAnalog("left", readPaddle(left));
-            state.SetAnalog("right", readPaddle(right));
-
+            state.SetAnalog("left", readPaddle(packet[2]));
+           
             return state.Build();
         }
     }
--- experimental/Paddles/Paddles.ino
+++ experimental/Paddles/Paddles.ino
@@ -5,56 +5,44 @@
 
 // --- These numbers will likely need modified ---
 
-// **percent_change_required_to_move** is the percentage of change required to move the paddle display.  
-// The lower the number the more jitter the higher the number the less jitter, 
-// but you will get less fluid movement in the display.  This number needs to be between 0 and 100.
-
-// **nominal_left_min/nominal_right_min** is the minimum value the left/right paddle is capable of hitting.  
+// **nominal_min** is the minimum value the paddle is capable of hitting.  
 // If the display is never hitting all the way left the value needs to be increased.  
 // If its hitting left too soon it needs to be decreased.  
 // The minimum value that can be selected is 0 and the maximum value should be less than **nominal_left_max/nominal_right_max**. 
 
-// **nominal_left_max/nominal_right_max** is the maximum value the left/right paddle is capable of hitting.  
+// **nominal_max** is the maximum value the paddle is capable of hitting.  
 // If the display is never hitting all the way right the value needs to be decreased.  
 // If its hitting right too soon it needs to be increased.  
 // The maximum value that can be selected is 1023 and the minimum value should be more than **nominal_left_min/nominal_right_min**. 
 
-// **EMA_al/EMA_ar** is the number of samples to average in order to smooth the signal.  1.0 is absolutely no smoothing.  
+// **EMA_a** is the number of samples to average in order to smooth the signal.  1.0 is absolutely no smoothing.  
 // The larger the number the more smoothing will occur at the cost of latency to the display.
 
-int percent_change_required_to_move = 5;
-int nominal_left_min = 213;
-int nominal_right_min = 207;
-int nominal_left_max = 1004;
-int nominal_right_max = 1003;
-float EMA_al = 100;      
-float EMA_ar = 100;
+int nominal_min = 213;
+int nominal_max = 1004;
+float EMA_a = 5;   
 
 // ---------- Uncomment for debugging output --------------
 //#define DEBUG
 
-// PINOUTS
-// Atari Pin 3 -> Digital Pin 4
+// PINOUTS for Right Paddle
 // Atari Pin 4 -> Digital Pin 5
 // Atari Pin 5 -> Analog Pin 0
 // Atari Pin 8 -> Arduino GND
-// Atari Pin 9 -> Analog Pin 1
+
+// PINOUTS for Left Paddle
+// Atari Pin 3 -> Digital Pin 5
+// Atari Pin 8 -> Arduino GND
+// Atari Pin 9 -> Analog Pin 0
 
 #define PIN_READ( pin )  (PIND&(1<<(pin)))
 
-int lastLeft = 0;
-bool leftAscending = false;
-int currentLeft = 0;
+volatile int lastVal = 0;
+volatile int currentVal = 0;
+volatile int analogVal = 0;
+volatile int readFlag;
 
-int lastRight = 0;
-bool rightAscending = false;
-int currentRight = 0;
-
-float EMA_Sl = 0;          //initialization of EMA S left
-float EMA_Sr = 0;          //initialization of EMA S right
-
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+float EMA_S = 0;          //initialization of EMA_S 
 
 static int ScaleInteger(float oldValue, float oldMin, float oldMax, float newMin, float newMax)
 {
@@ -67,99 +55,120 @@ static int ScaleInteger(float oldValue, float oldMin, float oldMax, float newMin
   return newValue;
 }
 
-void setup() {
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect)
+{
+  // Must read low first
+  analogVal = ADCL | (ADCH << 8);
 
-  sbi(ADCSRA, ADPS2);
-  cbi(ADCSRA, ADPS1);
-  cbi(ADCSRA, ADPS0);
+  if (analogVal < lastVal && (lastVal - analogVal) > 20)
+  {
+    currentVal = lastVal;
+    lastVal = analogVal;
+    readFlag = 1;
+  }
+  else
+  {
+    lastVal = analogVal;
+  }
+ 
+  // Not needed because free-running mode is enabled.
+  // Set ADSC in ADCSRA (0x7A) to start another ADC conversion
+  // ADCSRA |= B01000000;
+}
+
+void setup() {
   
   for (int i = 2; i <= 8; ++i)
     pinMode(i, INPUT_PULLUP);
 
-  EMA_Sl = currentLeft = analogRead(0);
-  EMA_Sr = currentRight = analogRead(1);
+  EMA_S = analogRead(0);
+
+  // clear ADLAR in ADMUX (0x7C) to right-adjust the result
+  // ADCL will contain lower 8 bits, ADCH upper 2 (in last two bits)
+  ADMUX &= B11011111;
+ 
+  // Set REFS1..0 in ADMUX (0x7C) to change reference voltage to the
+  // proper source (01)
+  ADMUX |= B01000000;
+ 
+  // Clear MUX3..0 in ADMUX (0x7C) in preparation for setting the analog
+  // input
+  ADMUX &= B11110000;
+ 
+  // Set MUX3..0 in ADMUX (0x7C) to read from AD8 (Internal temp)
+  // Do not set above 15! You will overrun other parts of ADMUX. A full
+  // list of possible inputs is available in Table 24-4 of the ATMega328
+  // datasheet
+  ADMUX |= 0;
+  // ADMUX |= B00001000; // Binary equivalent
+ 
+  // Set ADEN in ADCSRA (0x7A) to enable the ADC.
+  // Note, this instruction takes 12 ADC clocks to execute
+  ADCSRA |= B10000000;
+ 
+  // Set ADATE in ADCSRA (0x7A) to enable auto-triggering.
+  ADCSRA |= B00100000;
+ 
+  // Clear ADTS2..0 in ADCSRB (0x7B) to set trigger mode to free running.
+  // This means that as soon as an ADC has finished, the next will be
+  // immediately started.
+  ADCSRB &= B11111000;
+ 
+  // Set the Prescaler to 128 (16000KHz/128 = 125KHz)
+  // Above 200KHz 10-bit results are not reliable.
+  ADCSRA |= B00000111;
+ 
+  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
+  // Without this, the internal interrupt will not trigger.
+  ADCSRA |= B00001000;
+ 
+  // Enable global interrupts
+  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
+  // supplies by default.
+  sei();
+ 
+  // Kick off the first ADC
+  readFlag = 0;
+  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+  ADCSRA |=B01000000;
 
   Serial.begin( 115200 );
 }
 
 void loop() {
 
+  if (readFlag == 1)
+  {
 
-  noInterrupts();
-  byte pins = 0;
-  pins |= (PIND >> 2);
-  int leftPaddle = analogRead(0);
-  int rightPaddle = analogRead(1);
-  interrupts();
-
-  if (leftAscending && leftPaddle < lastLeft)
-  {
-    leftAscending = false;
-    currentLeft = lastLeft;
-    lastLeft = leftPaddle;
-    leftPaddle = currentLeft;
-  }
-  else if (!leftAscending && leftPaddle > lastLeft)
-  {
-    leftAscending = true;
-    lastLeft = leftPaddle;
-    leftPaddle = currentLeft;
-  }
-  else
-  {
-    lastLeft = leftPaddle;
-    leftPaddle = currentLeft;
-  }
-
-  if (rightAscending && rightPaddle < lastRight)
-  {
-    rightAscending = false;
-    currentRight = lastRight;
-    lastRight = rightPaddle;
-    rightPaddle = currentRight;
-  }
-  else if (!rightAscending && leftPaddle > lastLeft)
-  {
-    rightAscending = true;
-    lastRight = rightPaddle;
-    rightPaddle = currentRight;
-  }
-  else
-  {
-    lastRight = rightPaddle;
-    rightPaddle = currentRight;
-  }
+	byte pins = 0;
+	pins |= (PIND >> 2);
       
-  byte fire1 = ((pins & 0b0000000000000100) == 0);
-  byte fire2 = ((pins & 0b0000000000001000) == 0);
-  float mult_l = 2.0/(EMA_al + 1.0);
-  float mult_r = 2.0/(EMA_ar + 1.0);
-  EMA_Sl = (((float)leftPaddle) - EMA_Sl) * mult_l + EMA_Sl;
-  EMA_Sr = (((float)rightPaddle) - EMA_Sr) * mult_r + EMA_Sr;       
-  //EMA_Sl = (EMA_al*leftPaddle) + ((1-EMA_al)*EMA_Sl); 
-  //EMA_Sr = (EMA_ar*rightPaddle) + ((1-EMA_ar)*EMA_Sr);
+	byte fire2 = ((pins & 0b0000000000001000) == 0);
+	float mult = 2.0/(EMA_a + 1.0);
+	EMA_S = (((float)currentVal) - EMA_S) * mult + EMA_S;      
 #ifdef DEBUG
-    Serial.print(fire1 ? "3" : "-");
+    Serial.print("-");
     Serial.print(fire2 ? "4" : "-");
     Serial.print("|");
-    Serial.print(EMA_Sl);
+    Serial.print(EMA_S);
     Serial.print("|");
-    Serial.print(ScaleInteger(EMA_Sl, nominal_left_min, nominal_left_max, 0, 255));
+    Serial.print(ScaleInteger(EMA_S, nominal_min, nominal_max, 0, 255));
     Serial.print("|");
-    Serial.print(EMA_Sr);
+    Serial.print(0);
     Serial.print("|");
-    Serial.print(ScaleInteger(EMA_Sr, nominal_right_min, nominal_right_max, 0, 255));
+    Serial.print(0);
     Serial.print("\n");
 #else
-    int sil = ScaleInteger(EMA_Sl, nominal_left_min, nominal_left_max, 0, 255);
-    int sir = ScaleInteger(EMA_Sr, nominal_right_min, nominal_right_max, 0, 255);
-    Serial.write(fire1);
+    int sil = ScaleInteger(EMA_S, nominal_min, nominal_max, 0, 255);
+    Serial.write(0);
     Serial.write(fire2);
     Serial.write(sil);
-    Serial.write(sir);
+    Serial.write(0);
     Serial.write(5);
-    Serial.write(percent_change_required_to_move + 11);
+    Serial.write(11);
     Serial.write('\n');
 #endif
-
+	readFlag = 0;
+}
 }
