a7c1e0a4ea503e5b3d7584b46fed222088fb259d
Chris Mallery <chmaller@microsoft.com>
2019-10-21 09:02:17 -0700

    PS3?

--- InputSource.cs
+++ InputSource.cs
@@ -46,14 +46,15 @@ namespace RetroSpy
         static public readonly InputSource NEOGEO = new InputSource("neogeo", "SNK NeoGeo", true, false, false, port => new SerialControllerReader(port, NeoGeo.ReadFromPacket));
 
         static public readonly InputSource PLAYSTATION2 = new InputSource("playstation", "Sony Playstation 1/2", true, false, false, port => new SerialControllerReader(port, Playstation2.ReadFromPacket));
-        static public readonly InputSource PSCLASSIC = new InputSource("psclassic", "Sony PlayStation Classic", false, false, true, hostname => new SSHControllerReader(hostname, "sudo pkill -9 usb-mitm ; sudo usb-mitm 2> /dev/null -y", SuperNESandNES.ReadFromPacket_PSClassic));
+        static public readonly InputSource PS3 = new InputSource("playstation3", "Sony PlayStation 3", false, false, true, hostname => new SSHControllerReader(hostname, "sudo pkill -9 usb-mitm ; sudo usb-mitm 2> /dev/null -y", SuperNESandNES.ReadFromPacket_PSClassic));
+        static public readonly InputSource PSCLASSIC = new InputSource("psclassic", "Sony PlayStation Classic", false, false, true, hostname => new SSHControllerReader(hostname, "sudo pkill -9 usb-mitm ; sudo usb-mitm 2> /dev/null -u", PS3Reader.ReadFromPacket));
 
         //static public readonly InputSource PLAYSTATION = new InputSource("playstation", "Playstation", true, false, port => new SerialControllerReader(port, Playstation.ReadFromPacket));
         //static public readonly InputSource MOUSETESTER = new InputSource("mousetester", "Mouse Tester", true, false, port => new MouseTester(port));
         //static public readonly InputSource WII = new InputSource("wii", "Nintendo Wii", false, true, controllerId => new WiiReaderV1(int.Parse(controllerId)));
 
         static public readonly IReadOnlyList <InputSource> ALL = new List <InputSource> {
-            CLASSIC, DRIVINGCONTROLLER, ATARIKEYBOARD, PADDLES, CD32, INTELLIVISION, XBOX, XBOX360, TG16, NES, SNES, N64, GAMECUBE, WII, SWITCH, THREEDO, PC360, PAD, PADATOD, CDI, SEGA, SATURN3D, DREAMCAST, NEOGEO, PLAYSTATION2, PSCLASSIC
+            CLASSIC, DRIVINGCONTROLLER, ATARIKEYBOARD, PADDLES, CD32, INTELLIVISION, XBOX, XBOX360, TG16, NES, SNES, N64, GAMECUBE, WII, SWITCH, THREEDO, PC360, PAD, PADATOD, CDI, SEGA, SATURN3D, DREAMCAST, NEOGEO, PLAYSTATION2, PS3, PSCLASSIC
         };
 
         static public readonly InputSource DEFAULT = NES;
---
+++ Readers/PS3Reader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroSpy.Readers
+{
+    static public class PS3Reader
+    {
+        const int PACKET_SIZE = 152;
+        const int POLISHED_PACKET_SIZE = 40;
+
+        static readonly string[] BUTTONS = {
+            "square", "x", "circle", "triangle", "up", "right", "down", "left", "r1", "l1", "r2", "l2", "start", "r3", "l3", "select", null, null, null, null, null, null, null, "ps"
+        };
+
+
+        static float readAnalogButton(byte input)
+        {
+            return (float)input / 256;
+        }
+        static float readStick(byte input)
+        {
+            return (float)(input - 128) / 128;
+        }
+
+
+        static public ControllerState ReadFromPacket(byte[] packet)
+        {
+            if (packet.Length < PACKET_SIZE) return null;
+
+            byte[] polishedPacket = new byte[POLISHED_PACKET_SIZE];
+
+            for (int i = 0; i < 24; ++i)
+                polishedPacket[i] = (byte)((packet[i] == 0x31) ? 1 : 0);
+
+            for (int i = 0; i < 16; ++i)
+            {
+                polishedPacket[24 + i] = 0;
+                for (byte j = 0; j < 8; ++j)
+                {
+                    polishedPacket[24 + i] |= (byte)((packet[24 + (i * 8 + j)] == 0x30 ? 0 : 1) << j);
+                }
+            }
+
+            var outState = new ControllerStateBuilder();
+
+            for (int i = 0; i < BUTTONS.Length; ++i)
+            {
+                if (string.IsNullOrEmpty(BUTTONS[i])) continue;
+                outState.SetButton(BUTTONS[i], polishedPacket[i] != 0x00);
+            }
+
+            outState.SetAnalog("rstick_x", readStick(polishedPacket[26]));
+            outState.SetAnalog("rstick_y", readStick(polishedPacket[27]));
+            outState.SetAnalog("lstick_x", readStick(polishedPacket[24]));
+            outState.SetAnalog("lstick_y", readStick(polishedPacket[25]));
+
+            outState.SetAnalog("l_trig", readAnalogButton(polishedPacket[28]));
+            outState.SetAnalog("r_trig", readAnalogButton(polishedPacket[29]));
+
+            outState.SetAnalog("analog_right", readAnalogButton(polishedPacket[30]));
+            outState.SetAnalog("analog_left", readAnalogButton(polishedPacket[31]));
+            outState.SetAnalog("analog_up", readAnalogButton(polishedPacket[32]));
+            outState.SetAnalog("analog_down", readAnalogButton(polishedPacket[33]));
+            outState.SetAnalog("analog_triangle", readAnalogButton(polishedPacket[34]));
+            outState.SetAnalog("analog_circle", readAnalogButton(polishedPacket[35]));
+            outState.SetAnalog("analog_x", readAnalogButton(polishedPacket[36]));
+            outState.SetAnalog("analog_square", readAnalogButton(polishedPacket[37]));
+            outState.SetAnalog("analog_l1", readAnalogButton(polishedPacket[38]));
+            outState.SetAnalog("analog_r1", readAnalogButton(polishedPacket[39]));
+
+            return outState.Build();
+
+        }
+    }
+}
